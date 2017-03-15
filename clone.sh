fe49100cb714a967a69b70b9e0e29fc7ffb6f95a
adi <adi.papp@gmail.com>
2017-03-15 10:44:33 +0200

    First draft.

---
+++ clone.sh
@@ -0,0 +1,168 @@
+#unity
+
+git clone https://github.com/dario-zubovic/metaballs
+
+git clone https://github.com/keijiro/ColorSuite
+
+git clone https://github.com/sabresaurus/SabreCSG
+
+git clone https://github.com/slipster216/VertexPaint
+
+git clone https://github.com/SebLague/Procedural-Cave-Generation
+
+git clone https://github.com/SebLague/Procedural-Landmass-Generation.git
+
+git clone https://github.com/SebLague/Pathfinding-2D
+
+git clone https://github.com/SebLague/Create-a-Game-Source
+
+git clone https://github.com/SebLague/Object-Pooling
+
+git clone https://github.com/SebLague/Procedural-Landmass-Textures
+
+git clone https://github.com/SebLague/Field-of-View
+
+git clone https://github.com/SebLague/Blender-to-Unity-Character-Creation
+
+git clone https://github.com/SebLague/Kinematic-Equation-Problems
+
+git clone https://github.com/SebLague/Chess
+
+git clone https://github.com/procore3d/giles
+
+git clone https://github.com/poeticmatter/RoguelikeCore
+
+git clone https://github.com/vilbeyli/Minesweeper
+
+git clone https://github.com/sschmid/Entitas-CSharp.git
+
+git clone https://github.com/Unity-Technologies/PostProcessing
+
+git clone https://github.com/jongallant/DungeonGenerator
+
+git clone https://github.com/adamgit/Unity-delaunay
+
+git clone https://github.com/GreatArcStudios/UnityIndieEffects
+
+git clone https://github.com/GreatArcStudios/unitypausemenu
+
+git clone https://github.com/Demigiant/demilib
+
+git clone https://github.com/AK-Saigyouji/Procedural-Cave-Generator
+
+git clone https://github.com/reignstudios/Reign-Unity-Plugin5
+
+git clone https://github.com/vilbeyli/Pacman
+
+git clone https://github.com/federicocasares/stratecam
+
+git clone https://github.com/vicero/Procedural-Level-Engine-for-Unity
+
+git clone https://github.com/bab178/ProceduralCaveGenerationUnity
+
+git clone https://github.com/studiobytestorm/Draw-Call-Optimizer
+
+git clone https://github.com/thefuntastic/Unity3d-Finite-State-Machine
+
+git clone https://github.com/Syomus/ProceduralToolkit
+
+git clone https://github.com/TouchScript/TouchScript
+
+git clone https://github.com/nickgravelyn/unitytoolbag
+
+git clone https://bitbucket.org/NathisGreen/pcgdungeons.git
+
+git clone https://github.com/handcircus/Unity-Resource-Checker
+
+git clone https://github.com/AK-Saigyouji/Procedural-Cave-Generator
+
+git clone https://github.com/miketucker/Unity-Accidental-Noise
+
+git clone https://github.com/lanius/dynagon
+
+git clone https://github.com/x0r04rg/VisualNoiseDesigner
+
+git clone https://github.com/ricardojmendez/LibNoise.Unity
+
+git clone https://github.com/TrevorKinsie/ProcGenTest
+
+git clone https://github.com/brnkhy/MapzenGo
+
+git clone https://github.com/SamMurphy/Terrain-Generation-in-Unity
+
+git clone https://github.com/thadeaus/ProceduralGeneration
+
+git clone https://github.com/bryanrtboy/Flocking
+
+git clone https://github.com/nobnak/CurlNoiseUnity
+
+git clone https://github.com/ChemiKhazi/UnityPixelCamera
+
+git clone https://github.com/ChemiKhazi/PolyMesh
+
+#cpp
+
+git clone https://github.com/raysan5/raylib
+
+git clone https://github.com/prideout/heman
+
+git clone https://github.com/nothings/stb
+
+git clone https://github.com/Q42/0hh1
+
+git clone https://github.com/TurningWheel/Barony
+
+git clone https://github.com/Maescool/Catacomb-Snatch
+
+git clone https://github.com/Aransentin/RT-Engine
+
+git clone https://github.com/vrum/fractalterraingeneration
+
+git clone https://github.com/FabriceCastel/raytracer
+
+git clone https://github.com/Tecla/Rayito
+
+git clone https://github.com/sharpaccent/Astar-for-Unity
+
+#roguelike
+
+git clone https://github.com/flend/roguelike
+
+git clone https://bitbucket.org/clarktravism/rlnet.git
+
+git clone https://bitbucket.org/FaronBracy/roguesharp.git
+
+git clone https://bitbucket.org/dmhagar/empyrea-public.git
+
+hg clone https://bitbucket.org/cfyzium/bearlibterminal       #hg
+
+#wow
+
+hg clone https://bitbucket.org/wowmodelviewer/wowmodelviewer #hg
+
+git clone https://bitbucket.org/Mjollna/gillijimproject.git
+
+git clone https://bitbucket.org/will0x539/adt-converter.git
+
+#libgdx
+
+git clone https://github.com/jrenner/gdx-proto
+
+git clone https://github.com/haedri/shadow-mapping
+
+git clone https://github.com/manuelbua/libgdx-contribs
+
+git clone https://github.com/ImXico/HandyGDX
+
+#other
+
+git clone https://bitbucket.org/dalerank/caesaria.git
+
+git clone https://bitbucket.org/wlandry/sugrsonic.git
+
+#raytracer written using Go
+
+git clone https://github.com/fogleman/pt
+
+
+
